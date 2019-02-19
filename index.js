094eb9cb9e46e6e5d0d74e8bd5531fa2f88a034e
jchaselubitz <jchaselubitz@gmail.com>
2019-02-19 20:04:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+import { listenerCount } from "cluster";
+import { prototype } from "mocha";
 
+function getFirstSelector(selector) {
+    return document.querySelector(selector)
+}
+
+function nestedTarget() {
+    return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+    const lists = document.querySelectorAll('.ranked-list li')
+    for (let i = 0; i < lists.length; i++){
+        lists[i].innerHTML = (parseInt(lists[i].innerHTML) + n).toString()
+    }
+}
+
+// function deepestChild() {
+//     let findChild
+//     let deepener = "div"
+//     do {
+//         findChild = document.querySelector(`#grand-node ${deepener}`)
+//         deepener = deepener + " div"
+//     } while (findChild === document.querySelector(`#grand-node div`))
+//     return findChild
+// }
+
+function deepestChild() {
+    let node = document.querySelector(`#grand-node`)
+    let nextNode = node.children[0]
+
+    while (nextNode) {
+        node = nextNode
+        nextNode = node.children[0]
+    }
+    return node
+}
--- test/index-test.js
+++ test/index-test.js
@@ -14,7 +14,7 @@ describe('index', () => {
 
   describe('deepestChild()', () => {
     it('returns the most deeply nested child in #grand-node', () => {
-      console.log(deepestChild().innerHTML)
+      console.log(deepestChild().findChild)
       expect(deepestChild()).to.equal(document.querySelector('#grand-node div div div div'))
     })
   })
